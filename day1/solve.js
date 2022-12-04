10d88195a77f9548a1414e74c17d11d2f708e30b
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-04 16:43:45 +0100

    day 1 (add sample)

--- day1/solve.js
+++ day1/solve.js
@@ -1,14 +1,18 @@
 import _ from "lodash"
-import data from "./data.js"
+import data, { sample } from "./data.js"
 
 console.log("🎄 Day 1")
 
 /// Part 1
 
+const sRes1 = _.max(_.map(sample, _.sum))
 const res1 = _.max(_.map(data, _.sum))
-console.log("Part 1:", res1)
+
+console.log("Sample:", sRes1, "Task:", res1)
 
 /// Part 2
 
+const sRes2 = _.sum(_.take(_.sortBy(_.map(sample, _.sum)).reverse(), 3))
 const res2 = _.sum(_.take(_.sortBy(_.map(data, _.sum)).reverse(), 3))
-console.log("Part 2:", res2)
+
+console.log("Sample:", sRes2, "Task:", res2)
