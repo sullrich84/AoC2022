6545fb307dbd9770e2a0bbb188d5142d63d1eb48
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-04 16:48:24 +0100

    add blank template

---
+++ template/solve.js
@@ -0,0 +1,26 @@
+import _ from "lodash"
+import data, { sample } from "./data.js"
+
+console.log("🎄 Day X")
+
+/// Part 1
+
+const solve1 = (_) => {
+  return 0
+}
+
+const sRes1 = _.sum(sample.map(solve1))
+const res1 = 0 //_.sum(data.map(solve1))
+
+console.log("Sample:", sRes1, "Task:", res1)
+
+/// Part 2
+
+const solve2 = (_) => {
+  return 0
+}
+
+const sRes2 = _.sum(sample.map(solve2))
+const res2 = 0 //_.sum(data.map(solve2))
+
+console.log("Sample:", sRes2, "Task:", res2)
