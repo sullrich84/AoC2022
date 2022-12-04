a71479316b804356e006d484ab657c7d57f31cc0
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-04 09:53:57 +0100

    day 4

--- day4/solve.js
+++ day4/solve.js
@@ -5,22 +5,18 @@ console.log("🎄 Day 4")
 
 /// Part 1
 
-const solve1 = (content) => {
-  return 0
-}
+const solve1 = ([[s1s, s1e], [s2s, s2e]]) => ((s1s <= s2s && s1e >= s2e) || (s2s <= s1s && s2e >= s1e) ? 1 : 0)
 
-const sRes1 = sample.map(solve1)
-const res1 = data.map(solve1)
+const sRes1 = _.sum(sample.map(solve1))
+const res1 = _.sum(data.map(solve1))
 
 console.log("Sample:", sRes1, "Task:", res1)
 
 /// Part 2
 
-const solve2 = (content) => {
-  return 0
-}
+const solve2 = ([[s1s, s1e], [s2s, s2e]]) => (s1e < s2s || s2e < s1s ? 0 : 1)
 
-const sRes2 = sample.map(solve2)
-const res2 = data.map(solve2)
+const sRes2 = _.sum(sample.map(solve2))
+const res2 = _.sum(data.map(solve2))
 
 console.log("Sample:", sRes2, "Task:", res2)
