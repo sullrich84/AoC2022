7dda67c2dd88fb3c41f44ffff79eeeaf7470870a
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-12 17:02:55 +0100

    day 12

--- day12/solve.js
+++ day12/solve.js
@@ -8,19 +8,15 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 const solve1 = (ctx) => {
   let map = []
-  let visit = []
 
   let startY,
-    startX,
-    endY,
-    endX = 0
+    startX = 0
 
   // Set height and visit data
   ctx.data.forEach((row, y) => {
     map[y] = []
-    visit[y] = []
     row.split("").forEach((heightChar, x) => {
-      const pointInfo = { x, y }
+      const pointInfo = { x, y, id: [y, x].join(":") }
       switch (heightChar) {
         case "S":
           pointInfo.start = true
@@ -31,8 +27,6 @@ const solve1 = (ctx) => {
         case "E":
           pointInfo.end = true
           pointInfo.height = alphabet.indexOf("z")
-          endY = y
-          endX = x
           break
         default:
           pointInfo.height = alphabet.indexOf(heightChar)
@@ -40,17 +34,16 @@ const solve1 = (ctx) => {
       }
 
       map[y][x] = pointInfo
-      visit[y][x] = 0
     })
   })
 
   // Set neighbours
   map.forEach((row, y) => {
     row.forEach((__, x) => {
-      const top = _.inRange(y - 1, 0, map.length) ? map[y - 1][x] : null
-      const down = _.inRange(y + 1, 0, map.length) ? map[y + 1][x] : null
-      const left = _.inRange(x - 1, 0, row.length) ? map[y][x - 1] : null
-      const right = _.inRange(x + 1, 0, row.length) ? map[y][x + 1] : null
+      const top = _.inRange(y - 1, 0, map.length) ? { ...map[y - 1][x], name: "top" } : null
+      const down = _.inRange(y + 1, 0, map.length) ? { ...map[y + 1][x], name: "down" } : null
+      const left = _.inRange(x - 1, 0, row.length) ? { ...map[y][x - 1], name: "left" } : null
+      const right = _.inRange(x + 1, 0, row.length) ? { ...map[y][x + 1], name: "right" } : null
 
       map[y][x] = {
         ...map[y][x],
@@ -62,46 +55,65 @@ const solve1 = (ctx) => {
     })
   })
 
-  const findWay = (y, x, attempt) => {
-    visit[y][x] += 1
-    if (attempt >= 5000 || (x == endX && y == endY)) return attempt
+  // const findWay = (y, x, attempt) => {
+  //   visit[y][x] += 1
+  //   if (attempt >= 5000 || (x == endX && y == endY)) return attempt
 
-    const nextDest = (currentY, currentX) => {
-      const position = map[currentY][currentX]
-      var dest = [position.top, position.down, position.left, position.right].filter((p) => p != null)
+  //   const nextDest = (currentY, currentX) => {
+  //     const position = map[currentY][currentX]
+  //     var dest = [position.top, position.down, position.left, position.right].filter((p) => p != null)
 
-      dest.map((p) => {
-        p.visit = visit[p.y][p.x]
-        return p
-      })
+  //     dest.map((p) => {
+  //       p.visit = visit[p.y][p.x]
+  //       return p
+  //     })
 
-      dest = dest.map((p) => {
-        if (p.height <= position.height + 1) {
-          return p
-        } else {
-          return null
-        }
-      })
+  //     dest = dest.filter((p) => {
+  //       return p.height <= position.height + 1
+  //     })
 
-      dest = dest.filter((p) => p != null)
+  //     dest.sort((p1, p2) => p2.height - p1.height)
+  //     // dest.sort((p1, p2) => p1.visit - p2.visit)
+  //     dest = dest.filter((p) => p.visit < visit[position.y][position.x])
 
-      dest.sort((p1, p2) => p2.height - p1.height)
-      dest.sort((p1, p2) => p1.visit - p2.visit)
-      return _.first(dest)
+  //     return dest
+  //   }
+
+  //   const destinations = nextDest(y, x)
+
+  //   const a = destinations.map((d) => findWay(d.y, d.x, attempt + 1)).filter(d => d!=null)
+
+  //   const dest = _.min(a)
+  //   //return findWay(dest.y, dest.x, attempt + 1)
+  // }
+
+  var reacedTargetWithSteps = []
+
+  const findWay = (y, x, counter, history) => {
+    const pos = map[y][x]
+    history.push(pos.id)
+
+    if (pos.end) {
+      reacedTargetWithSteps.push(counter)
+    } else {
+      var neighbours = [pos.top, pos.down, pos.left, pos.right]
+        .filter((n) => n != null)
+        .filter((n) => !history.includes(n.id))
+        .filter((n) => n.height <= pos.height + 1)
+        .sort((p1, p2) => p2.height - p1.height)
+
+      for (const neighbour of neighbours) {
+        findWay(neighbour.y, neighbour.x, counter + 1, [...history])
+      }
     }
-
-    const dest = nextDest(y, x)
-    return findWay(dest.y, dest.x, attempt + 1)
   }
 
-  const runs = findWay(startY, startX, 0)
-  return runs
+  findWay(startY, startX, 0, [])
+  return _.min(reacedTargetWithSteps)
 }
 
-const sRes1 = [{ data: sample }].map(solve1)
-const res1 = [{ data: data }].map(solve1)
-
-console.log("Sample:", sRes1, "Task:", res1)
+console.log("Sample:", [{ data: sample }].map(solve1))
+// console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
