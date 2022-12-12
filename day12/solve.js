430c28c576d9e6f2766880c4667b259ed00998da
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-12 18:11:15 +0100

    day 12

--- day12/solve.js
+++ day12/solve.js
@@ -8,36 +8,22 @@ console.log("🎄 Day 12")
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 const solve1 = (ctx) => {
-  let map = []
-
-  var startId = ""
-  var endId = ""
+  const map = []
+  const route = new Graph()
 
   ctx.data.forEach((row, y) => {
     map[y] = []
     row.split("").forEach((heightChar, x) => {
-      const pointInfo = { x, y, id: [y, x].join(":") }
-      switch (heightChar) {
-        case "S":
-          pointInfo.start = true
-          pointInfo.height = alphabet.indexOf("a")
-          startId = pointInfo.id
-          break
-        case "E":
-          pointInfo.end = true
-          pointInfo.height = alphabet.indexOf("z")
-          endId = pointInfo.id
-          break
-        default:
-          pointInfo.height = alphabet.indexOf(heightChar)
-          break
+      map[y][x] = { x, y, id: [y, x].join(":") }
+      if (heightChar === "S") {
+        map[y][x] = { ...map[y][x], start: true, height: alphabet.indexOf("a") }
+      } else if (heightChar === "E") {
+        map[y][x] = { ...map[y][x], end: true, height: alphabet.indexOf("z") }
+      } else {
+        map[y][x] = { ...map[y][x], height: alphabet.indexOf(heightChar) }
       }
-
-      map[y][x] = pointInfo
     })
   })
-
-  const route = new Graph()
 
   // Set neighbours
   map.forEach((row, y) => {
@@ -46,14 +32,6 @@ const solve1 = (ctx) => {
       const down = _.inRange(y + 1, 0, map.length) ? { ...map[y + 1][x], name: "down" } : null
       const left = _.inRange(x - 1, 0, row.length) ? { ...map[y][x - 1], name: "left" } : null
       const right = _.inRange(x + 1, 0, row.length) ? { ...map[y][x + 1], name: "right" } : null
-
-      map[y][x] = {
-        ...map[y][x],
-        top,
-        down,
-        left,
-        right,
-      }
 
       var neighbours = {}
       if (top && top.height <= map[y][x].height + 1) neighbours[top.id] = 1
@@ -65,45 +43,36 @@ const solve1 = (ctx) => {
     })
   })
 
-  return route.path(startId, endId).length - 1
+  const flatMap = _.flatten(map)
+  const start = flatMap.find((p) => p.start)
+  const end = flatMap.find((p) => p.end)
+  return route.path(start.id, end.id).length - 1
 }
 
-console.log("Sample:", [{ data: sample }].map(solve1))
-console.log("Task:", [{ data: data }].map(solve1))
+const sRes1 = [{ data: sample }].map(solve1)
+const res1 = [{ data: data }].map(solve1)
+
+console.log("Sample:", sRes1, "Task:", res1)
 
 /// Part 2
 
 const solve2 = (ctx) => {
-  let map = []
-
-  var startId = ""
-  var endId = ""
+  const map = []
+  const route = new Graph()
 
   ctx.data.forEach((row, y) => {
     map[y] = []
     row.split("").forEach((heightChar, x) => {
-      const pointInfo = { x, y, id: [y, x].join(":") }
-      switch (heightChar) {
-        case "S":
-          pointInfo.start = true
-          pointInfo.height = alphabet.indexOf("a")
-          startId = pointInfo.id
-          break
-        case "E":
-          pointInfo.end = true
-          pointInfo.height = alphabet.indexOf("z")
-          endId = pointInfo.id
-          break
-        default:
-          pointInfo.height = alphabet.indexOf(heightChar)
-          break
+      map[y][x] = { x, y, id: [y, x].join(":") }
+      if (heightChar === "S") {
+        map[y][x] = { ...map[y][x], start: true, height: alphabet.indexOf("a") }
+      } else if (heightChar === "E") {
+        map[y][x] = { ...map[y][x], end: true, height: alphabet.indexOf("z") }
+      } else {
+        map[y][x] = { ...map[y][x], height: alphabet.indexOf(heightChar) }
       }
-
-      map[y][x] = pointInfo
     })
   })
-
-  const route = new Graph()
 
   // Set neighbours
   map.forEach((row, y) => {
@@ -112,14 +81,6 @@ const solve2 = (ctx) => {
       const down = _.inRange(y + 1, 0, map.length) ? { ...map[y + 1][x], name: "down" } : null
       const left = _.inRange(x - 1, 0, row.length) ? { ...map[y][x - 1], name: "left" } : null
       const right = _.inRange(x + 1, 0, row.length) ? { ...map[y][x + 1], name: "right" } : null
-
-      map[y][x] = {
-        ...map[y][x],
-        top,
-        down,
-        left,
-        right,
-      }
 
       var neighbours = {}
       if (top && top.height <= map[y][x].height + 1) neighbours[top.id] = 1
@@ -131,13 +92,18 @@ const solve2 = (ctx) => {
     })
   })
 
-  const points = _.flatten(map)
-    .filter((p) => p.height == 0)
-    .map((p) => route.path(p.id, endId)?.length - 1 || null)
-    .filter((p) => p != null)
+  const flatMap = _.flatten(map)
+  const end = flatMap.find((p) => p.end)
 
-  return _.min(points)
+  return flatMap
+    .filter((p) => p.height == 0)
+    .map((p) => route.path(p.id, end.id)?.length - 1 || null)
+    .filter((p) => p != null)
+    .sort((a, b) => a - b)
+    .shift()
 }
 
-console.log("Sample:", [{ data: sample }].map(solve2))
-console.log("Task:", [{ data: data }].map(solve2))
+const sRes2 = [{ data: sample }].map(solve2)
+const res2 = [{ data: data }].map(solve2)
+
+console.log("Sample:", sRes2, "Task:", res2)
