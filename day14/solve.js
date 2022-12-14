88cc1d0cd44477d7d0132e3b139c9ad4059915cb
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-14 08:10:02 +0100

    day 14

---
+++ day14/solve.js
@@ -0,0 +1,113 @@
+import _ from "lodash"
+import data, { sample } from "./data.js"
+
+console.log("🎄 Day 14")
+
+function drawMap(map) {
+  console.clear()
+  map.forEach((row) => {
+    console.log(row.join(""))
+  })
+}
+
+function pairwise(arr, func) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    func(arr[i], arr[i + 1])
+  }
+}
+
+/// Part 1
+
+const solve1 = (input) => {
+  console.log(input)
+  return 0
+}
+
+const sRes1 = 0 //_.sum(sample.map(solve1))
+const res1 = 0 //_.sum(data.map(solve1))
+
+console.log("Sample:", sRes1, "Task:", res1)
+
+/// Part 2
+
+const solve2 = (ctx) => {
+  const flattenData = [..._.flatten(ctx.data), [500, 0]]
+  const minX = _.min(flattenData.map(([x, y]) => x)) - 2 // allows sand flow aside
+  const maxX = _.max(flattenData.map(([x, y]) => x)) + 2 // allows sand flow aside
+  const minY = _.min(flattenData.map(([x, y]) => y))
+  const maxY = _.max(flattenData.map(([x, y]) => y))
+
+  const map = _.times(maxY - minY + 1, () => new Array(maxX - minX + 1).fill("."))
+
+  const setRock = (x, y) => {
+    const dx = x - minX
+    const dy = y - minY
+    // if (!map[dy]) map[dy] = []
+    map[dy][dx] = "#"
+  }
+
+  ctx.data.forEach((input) => {
+    pairwise(input, ([x1, y1], [x2, y2]) => {
+      const xS = _.min([x1, x2])
+      const xE = _.max([x1, x2])
+      const yS = _.min([y1, y2])
+      const yE = _.max([y1, y2])
+
+      for (var x = xS; x <= xE; x++) {
+        for (var y = yS; y <= yE; y++) {
+          setRock(x, y)
+        }
+      }
+    })
+  })
+
+  const get = (x, y) => {
+    const dx = x - minX
+    const dy = y - minY
+    return map[dy][dx]
+  }
+
+  const set = (x, y, v) => {
+    const dx = x - minX
+    const dy = y - minY
+    map[dy][dx] = v
+  }
+
+  let rest = 0
+
+  _.times(24, () => {
+    let x = 500 // right
+    let y = 0 // down
+
+    while (y < maxY) {
+      const nextPos = get(x, y + 1)
+      if (nextPos !== ".") {
+        // Block below is blocked
+        const nextPosLeft = get(x - 1, y + 1)
+        const nextPosRight = get(x + 1, y + 1)
+
+        if (nextPosLeft === ".") {
+          x -= 1
+        } else if (nextPosRight === ".") {
+          x += 1
+        } else {
+          // Both ways arround blocked; place at current
+          set(x, y, "o")
+          rest += 1
+          break
+        }
+      }
+
+      y += 1
+    }
+    
+  })
+
+  drawMap(map)
+  return rest
+}
+
+const sRes2 = [{ data: sample }].map(solve2)
+const res2 = [{ data: data }].map(solve2)
+
+console.log("Sample:", sRes2, "Task:", res2)
