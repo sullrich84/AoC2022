52c5da45b9d16ab2817ae686a7c769c39e284d19
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-15 09:34:32 +0100

    day 15 (brute force)

--- day15/solve.js
+++ day15/solve.js
@@ -13,61 +13,58 @@ function drawMap(map) {
 /// Part 1
 
 const solve1 = (ctx) => {
-  const minX = _.min(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sx, bx])))
-  const maxX = _.max(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sx, bx])))
-  const minY = _.min(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sy, by])))
-  const maxY = _.max(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sy, by])))
+  // const minX = _.min(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sx, bx])))
+  // const maxX = _.max(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sx, bx])))
+  // const minY = _.min(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sy, by])))
+  // const maxY = _.max(_.flatten(ctx.data.map(([sx, sy, bx, by]) => [sy, by])))
 
-  var map = []
+  // var map = []
 
-  _.range(minY, maxY + 1).forEach((mx) => {
-    map[mx] = []
-    _.range(minX, maxX + 1).forEach((my) => {
-      map[mx][my] = "."
-    })
-  })
+  // _.range(minY, maxY + 1).forEach((mx) => {
+  //   map[mx] = []
+  //   _.range(minX, maxX + 1).forEach((my) => {
+  //     map[mx][my] = "."
+  //   })
+  // })
 
-  const get = (x, y) => {
-    return map[y][x]
-  }
+  // const get = (x, y) => {
+  //   return map[y][x]
+  // }
 
-  const set = (x, y, v) => {
-    map[y][x] = v
-  }
+  // const set = (x, y, v) => {
+  //   map[y][x] = v
+  // }
 
-  ctx.data.forEach(([sx, sy, bx, by]) => {
-    set(sx, sy, "S") // Set sensor
-    set(bx, by, "B") // Set beacon
-  })
+  // ctx.data.forEach(([sx, sy, bx, by]) => {
+  //   set(sx, sy, "S") // Set sensor
+  //   set(bx, by, "B") // Set beacon
+  // })
 
   const search = []
 
   ctx.data.forEach(([sx, sy, bx, by]) => {
     var distance = Math.abs(bx - sx) + Math.abs(by - sy)
+    var c = 0
 
-    var c = sy < ctx.target ? ctx.target - sy : 
-    const xMin = sx - c
-    const xMax = sx + c
+    const vMin = sy - distance // -2
+    const vMax = sy + distance // 16
+    const inTargetRange = vMin <= ctx.target && vMax >= ctx.target
 
-    const oldMin = _.get(search, `[${v}].min`, xMin)
-    const oldMax = _.get(search, `[${v}].max`, xMax)
+    if (inTargetRange) {
+      for (var v = sy - distance; v <= sy + distance; v++) {
+        if (v === ctx.target) {
+          const xMin = sx - c
+          const xMax = sx + c
 
-    _.set(search, `[${v}].min`, _.min([oldMin, xMin]))
-    _.set(search, `[${v}].max`, _.max([oldMax, xMax]))
+          const oldMin = _.get(search, `[${v}].min`, xMin)
+          const oldMax = _.get(search, `[${v}].max`, xMax)
 
-    // var c = 0
-
-    // for (var v = sy - distance; v <= sy + distance; v++) {
-    //   const xMin = sx - c
-    //   const xMax = sx + c
-
-    //   const oldMin = _.get(search, `[${v}].min`, xMin)
-    //   const oldMax = _.get(search, `[${v}].max`, xMax)
-
-    //   _.set(search, `[${v}].min`, _.min([oldMin, xMin]))
-    //   _.set(search, `[${v}].max`, _.max([oldMax, xMax]))
-    //   c = v < sy ? c + 1 : c - 1
-    // }
+          _.set(search, `[${v}].min`, _.min([oldMin, xMin]))
+          _.set(search, `[${v}].max`, _.max([oldMax, xMax]))
+        }
+        c = v < sy ? c + 1 : c - 1
+      }
+    }
   })
 
   const min = search[ctx.target].min
@@ -75,8 +72,8 @@ const solve1 = (ctx) => {
   return min > max ? min - max : max - min
 }
 
-const sRes1 = [{ data: sample, target: 10 }].map(solve1)
-const res1 = [{ data: data, target: 10 }].map(solve1)
+const sRes1 = 0// [{ data: sample, target: 10 }].map(solve1)
+const res1 = [{ data: data, target: 2000000 }].map(solve1)
 
 console.log("Sample:", sRes1, "Task:", res1)
 
