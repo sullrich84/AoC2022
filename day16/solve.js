6155dfb109662ac274ce2f66f3756665c2e2230f
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2022-12-16 08:47:27 +0100

    day 16

---
+++ day16/solve.js
@@ -0,0 +1,28 @@
+import _ from "lodash"
+import data, { sample } from "./data.js"
+
+console.log("🎄 Day 16")
+
+/// Part 1
+
+const solve1 = (input) => {
+  console.log(input)
+  return 0
+}
+
+const sRes1 = 0 //_.sum(sample.map(solve1))
+const res1 = 0 //_.sum(data.map(solve1))
+
+console.log("Sample:", sRes1, "Task:", res1)
+
+/// Part 2
+
+const solve2 = (input) => {
+  console.log(input)
+  return 0
+}
+
+const sRes2 = 0 //_.sum(sample.map(solve2))
+const res2 = 0 //_.sum(data.map(solve2))
+
+console.log("Sample:", sRes2, "Task:", res2)
