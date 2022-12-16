1832ec3899008eb4a61a1842f856ebba2f26d502
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2022-12-16 14:29:13 +0100

    day 16

--- day16/solve.js
+++ day16/solve.js
@@ -7,29 +7,68 @@ console.log("🎄 Day 16")
 /// Part 1
 
 const solve1 = (ctx) => {
-  const travelCosts = 1
-  const route = new Graph()
+  const valves = [...ctx.data].map((v) => {
+    v.open = false
+    return v
+  })
+
+  const graph = new Graph()
+
+  valves.forEach((v) => {
+    const neigbours = {}
+    v.nodes.forEach((n) => {
+      neigbours[n] = 1
+    })
+    graph.addNode(v.id, neigbours)
+  })
 
   const getNode = (id) => {
-    return _.first(ctx.data.filter(({ node }) => node === id))
+    return _.first(valves.filter((v) => v.id === id))
   }
 
-  ctx.data.forEach(({ node, nodes }) => {
-    const neighbours = {}
-    nodes.forEach((id) => {
-      const neighbour = getNode(id)
-      const flowCosts = 30 - neighbour.flowRate
-      neighbours[id] = travelCosts + flowCosts
+  const findNext = ({ id }, m) => {
+    const openValves = valves.filter((v) => !v.open)
+    const targets = openValves.map((t) => {
+      const path = graph.path(id, t.id)
+      return { ...t, path: path || [] }
     })
-    route.addNode(node, neighbours)
-  })
 
-  const [start, ...nodes] = ctx.data
-  const paths = nodes.map(({ node }) => {
-    return route.path(start.node, node)
-  })
+    const remainingTime = 30 - m
+    targets.sort((a, b) => {
+      const bv = (remainingTime - b.path.length) * b.flowRate
+      const av = (remainingTime - a.path.length) * a.flowRate
+      return bv - av
+    })
 
-  return 0
+    const bestTarget = _.first(targets)
+    if (bestTarget.id === id) {
+      return bestTarget
+    }
+
+    const nextId = bestTarget.path[1]
+    const next = getNode(nextId)
+    return next
+  }
+
+  var pressureSum = 0
+  var current = getNode("AA")
+
+  for (var m = 0; m < 30; m++) {
+    const openValves = valves.filter(({ open }) => open)
+    pressureSum += _.sum(openValves.map(({ flowRate }) => flowRate))
+
+    const dest = findNext(current, m)
+
+    if (current.id === dest.id && !current.open && current.flowRate >= 0) {
+      current.open = true
+      continue
+    }
+
+    // Move to next tunnel
+    current = dest
+  }
+
+  return pressure
 }
 
 const sRes1 = [{ data: sample }].map(solve1)
