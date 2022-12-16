860d637fbd6ba10bc45cba02806d520bc0550c8f
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-16 21:32:27 +0100

    day 16

--- day16/solve.js
+++ day16/solve.js
@@ -16,19 +16,38 @@ const solve1 = (ctx) => {
     return _.first(valves.filter((v) => v.id === id))
   }
 
-  const find = (current, timeLeft) => {
-    if (timeLeft <= 0) return 0
+  const cache = []
 
-    if(!current.open) {
-      
+  const findMaxPressure = (current, opened, timeLeft) => {
+    if (timeLeft <= 0 || opened.includes(current)) return 0
+
+    const key = current + opened.join("") + timeLeft
+    if (key in cache) return cache[key]
+
+    var best = 0
+    const currentNode = getNode(current)
+    const currentRelease = (timeLeft - 1) * currentNode.flowRate
+
+    for (const next of currentNode.nodes) {
+      if (currentRelease != 0) {
+        // Open current valve and move to next tunnel
+        const nextRelease = findMaxPressure(next, [...opened, current], timeLeft - 2)
+        best = _.max([best, currentRelease + nextRelease])
+      }
+
+      // Ignore current valve and continue walking to the next tunnels
+      best = _.max([best, findMaxPressure(next, opened, timeLeft - 1)])
     }
+
+    cache[key] = best
+    return best
   }
 
-  return pressure
+  return findMaxPressure("AA", [], 30)
 }
 
 const sRes1 = [{ data: sample }].map(solve1)
-const res1 = 0 //_.sum(data.map(solve1))
+const res1 = 0 // [{ data: data }].map(solve1)
 
 console.log("Sample:", sRes1, "Task:", res1)
 
