41caae23e9a6b67450df4f44101a87b034dad820
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2022-12-16 09:15:04 +0100

    day 16

--- day16/solve.js
+++ day16/solve.js
@@ -1,16 +1,38 @@
 import _ from "lodash"
 import data, { sample } from "./data.js"
+import Graph from "node-dijkstra"
 
 console.log("🎄 Day 16")
 
 /// Part 1
 
-const solve1 = (input) => {
-  console.log(input)
+const solve1 = (ctx) => {
+  const travelCosts = 1
+  const route = new Graph()
+
+  const getNode = (id) => {
+    return _.first(ctx.data.filter(({ node }) => node === id))
+  }
+
+  ctx.data.forEach(({ node, nodes }) => {
+    const neighbours = {}
+    nodes.forEach((id) => {
+      const neighbour = getNode(id)
+      const flowCosts = 30 - neighbour.flowRate
+      neighbours[id] = travelCosts + flowCosts
+    })
+    route.addNode(node, neighbours)
+  })
+
+  const [start, ...nodes] = ctx.data
+  const paths = nodes.map(({ node }) => {
+    return route.path(start.node, node)
+  })
+
   return 0
 }
 
-const sRes1 = 0 //_.sum(sample.map(solve1))
+const sRes1 = [{ data: sample }].map(solve1)
 const res1 = 0 //_.sum(data.map(solve1))
 
 console.log("Sample:", sRes1, "Task:", res1)
