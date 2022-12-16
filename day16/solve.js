976e155f7ed0bad48d701034ca5846c98e1ea463
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2022-12-16 16:47:15 +0100

    day 16

--- day16/solve.js
+++ day16/solve.js
@@ -12,60 +12,16 @@ const solve1 = (ctx) => {
     return v
   })
 
-  const graph = new Graph()
-
-  valves.forEach((v) => {
-    const neigbours = {}
-    v.nodes.forEach((n) => {
-      neigbours[n] = 1
-    })
-    graph.addNode(v.id, neigbours)
-  })
-
   const getNode = (id) => {
     return _.first(valves.filter((v) => v.id === id))
   }
 
-  const findNext = ({ id }, m) => {
-    const openValves = valves.filter((v) => !v.open)
-    const targets = openValves.map((t) => {
-      const path = graph.path(id, t.id)
-      return { ...t, path: path || [] }
-    })
+  const find = (current, timeLeft) => {
+    if (timeLeft <= 0) return 0
 
-    const remainingTime = 30 - m
-    targets.sort((a, b) => {
-      const bv = (remainingTime - b.path.length) * b.flowRate
-      const av = (remainingTime - a.path.length) * a.flowRate
-      return bv - av
-    })
-
-    const bestTarget = _.first(targets)
-    if (bestTarget.id === id) {
-      return bestTarget
+    if(!current.open) {
+      
     }
-
-    const nextId = bestTarget.path[1]
-    const next = getNode(nextId)
-    return next
-  }
-
-  var pressureSum = 0
-  var current = getNode("AA")
-
-  for (var m = 0; m < 30; m++) {
-    const openValves = valves.filter(({ open }) => open)
-    pressureSum += _.sum(openValves.map(({ flowRate }) => flowRate))
-
-    const dest = findNext(current, m)
-
-    if (current.id === dest.id && !current.open && current.flowRate >= 0) {
-      current.open = true
-      continue
-    }
-
-    // Move to next tunnel
-    current = dest
   }
 
   return pressure
