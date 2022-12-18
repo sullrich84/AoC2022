edadfb548fb06860166926e0074eb5dd27adf32d
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-18 16:08:04 +0100

    day 18 (part 2, unsolved)

--- day18/solve.js
+++ day18/solve.js
@@ -19,19 +19,69 @@ const solve1 = ({ data }) => {
   return _.sum(exposed)
 }
 
-const sRes1 = [{ data: sample }].map(solve1)
-const res1 = [{ data: data }].map(solve1)
+// const sRes1 = [{ data: sample }].map(solve1)
+// const res1 = [{ data: data }].map(solve1)
 
-console.log("Sample:", sRes1, "Task:", res1)
+// console.log("Sample:", sRes1, "Task:", res1)
 
 /// Part 2
 
-const solve2 = (input) => {
-  console.log(input)
-  return 0
+const solve2 = ({ data }) => {
+  // 0:21 grid
+  const xMin = _.min(data.map(([x, y, z]) => x))
+  const xMax = _.max(data.map(([x, y, z]) => x))
+  const yMin = _.min(data.map(([x, y, z]) => y))
+  const yMax = _.max(data.map(([x, y, z]) => y))
+  const zMin = _.min(data.map(([x, y, z]) => z))
+  const zMax = _.max(data.map(([x, y, z]) => z))
+
+  const blockCache = {}
+  const hasBlockAt = (x, y, z) => {
+    const key = [x, y, z].join(":")
+    if (key in blockCache) return blockCache[key]
+    const hasBlock = !!data.find(([ex, ey, ez]) => x === ex && y === ey && z === ez)
+    blockCache[key] = hasBlock
+    return hasBlock
+  }
+
+  const exposedCache = {}
+  const isExposed = (x, y, z) => {
+    const key = [x, y, z].join(":")
+    if (key in exposedCache) return exposedCache[key]
+
+    if (x <= xMin || x >= xMax || y <= yMin || y >= xMax || z <= zMin || z >= zMax) {
+      // Block is at the grids outside and therefore exposed
+      exposedCache[key] = true
+      return true
+    }
+
+    const exposed =
+      (!hasBlockAt(x - 1, y, z) && isExposed(x - 1, y, z)) ||
+      (!hasBlockAt(x + 1, y, z) && isExposed(x + 1, y, z)) ||
+      (!hasBlockAt(x, y + 1, z) && isExposed(x, y + 1, z)) ||
+      (!hasBlockAt(x, y - 1, z) && isExposed(x, y - 1, z)) ||
+      (!hasBlockAt(x, y, z + 1) && isExposed(x, y, z + 1)) ||
+      (!hasBlockAt(x, y, z - 1) && isExposed(x, y, z - 1))
+
+    exposedCache[key] = exposed
+    return exposed
+  }
+
+  const exposed = data.map(([x, y, z]) => {
+    const rr = hasBlockAt(x - 1, y, z) ? 0 : 1
+    const ll = hasBlockAt(x + 1, y, z) ? 0 : 1
+    const tt = hasBlockAt(x, y + 1, z) ? 0 : 1
+    const dd = hasBlockAt(x, y - 1, z) ? 0 : 1
+    const ff = hasBlockAt(x, y, z + 1) ? 0 : 1
+    const bb = hasBlockAt(x, y, z - 1) ? 0 : 1
+
+    return isExposed(x, y, z) ? rr + ll + tt + dd + ff + bb : 0
+  })
+
+  return _.sum(exposed)
 }
 
-const sRes2 = 0 //_.sum(sample.map(solve2))
-const res2 = 0 //_.sum(data.map(solve2))
+const sRes2 = [{ data: sample }].map(solve2)
+const res2 = [{ data: data }].map(solve2)
 
 console.log("Sample:", sRes2, "Task:", res2)
