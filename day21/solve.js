aefeb9cd50821b15704c0ca4f56cd60e4198a740
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-20 19:52:34 +0100

    day 21 (prep)

---
+++ day21/solve.js
@@ -0,0 +1,28 @@
+import _ from "lodash"
+import data, { sample } from "./data.js"
+
+console.log("🎄 Day 21")
+
+/// Part 1
+
+const solve1 = ({ data }) => {
+  console.log(data)
+  return 0
+}
+
+const sRes1 = [{ data: sample }].map(solve1)
+const res1 = 0 //[{ data: data }].map(solve1)
+
+console.log("Sample:", sRes1, "Task:", res1)
+
+/// Part 2
+
+const solve2 = (input) => {
+  console.log(input)
+  return 0
+}
+
+const sRes2 = 0 // [{ data: sample }].map(solve2)
+const res2 = 0 //[{ data: data }].map(solve2)
+
+console.log("Sample:", sRes2, "Task:", res2)
