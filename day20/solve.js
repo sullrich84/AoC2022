1b39a624c0da168eac0920a997236cce6c5e08f5
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-20 19:28:21 +0100

    day 20 (solved)

--- day20/solve.js
+++ day20/solve.js
@@ -45,12 +45,44 @@ console.log("Sample:", sRes1, "Task:", res1)
 
 /// Part 2
 
-const solve2 = (input) => {
-  console.log(input)
-  return 0
+const solve2 = ({ numbers }) => {
+  const decryptionKey = 811589153
+
+  var elements = numbers.map((val, id) => {
+    return { id, val: val * decryptionKey }
+  })
+
+  const len = elements.length
+
+  const swap = (array, elem) => {
+    if (elem.val === 0) return array
+    var temp = [...array]
+
+    const oldIdx = _.findIndex(array, ({ id }) => id === elem.id)
+    const newIdx = (oldIdx + elem.val) % (len - 1)
+
+    temp.splice(oldIdx, 1)
+    temp.splice(newIdx, 0, elem)
+
+    return temp
+  }
+
+  for (var m = 0; m < 10; m++) {
+    for (var i = 0; i < len; i++) {
+      const { id, val } = elements.filter(({ id }) => id === i)[0]
+      elements = swap(elements, { id, val })
+    }
+  }
+
+  const zero = elements.findIndex(({ val }) => val === 0)
+
+  return [1000, 2000, 3000]
+    .map((shift) => (zero + shift) % len)
+    .map((idx) => elements[idx].val)
+    .reduce((acc, val) => acc + val, 0)
 }
 
-const sRes2 = 0 // [{ data: sample }].map(solve2)
-const res2 = 0 //[{ data: data }].map(solve2)
+const sRes2 = [{ numbers: sample }].map(solve2)
+const res2 = [{ numbers: data }].map(solve2)
 
 console.log("Sample:", sRes2, "Task:", res2)
