fabcef6e87bc4389f7d2f086c29a143273ff6eee
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-25 10:33:27 +0100

    day 25 (part 1)

--- day25/solve.js
+++ day25/solve.js
@@ -5,24 +5,37 @@ console.log("🎄 Day 25")
 
 /// Part 1
 
-const toBase10 = (snafuNum) => {
+const toBase10 = (stringBaseSNAFU) => {
   const SNAFU = { 2: 2, 1: 1, 0: 0, "-": -1, "=": -2 }
-  return snafuNum
+  return stringBaseSNAFU
     .split("")
     .map((c, idx, num) => SNAFU[c] * Math.pow(5, num.length - idx - 1))
     .reduce((acc, val) => acc + val, 0)
 }
 
-const toSNAFU = (snafuNum) => {
-  const SNAFU = { 2: 2, 1: 1, 0: 0, "-": -1, "=": -2 }
-  return snafuNum
-    .split("")
-    .map((c, idx, num) => SNAFU[c] * Math.pow(5, num.length - idx - 1))
-    .reduce((acc, val) => acc + val, 0)
+const toBaseSNAFU = (intBase10) => {
+  var total = intBase10
+  var output = ""
+
+  while (total !== 0) {
+    var rem = total % 5
+    total = Math.floor(total / 5)
+
+    if (rem <= 2) {
+      output = rem.toString() + output
+    } else {
+      output = "   =-".charAt(rem) + output
+      total += 1
+    }
+  }
+
+  return output
 }
 
 const solve1 = ({ data }) => {
-  return _.sum(data.map(toBase10))
+  const base10Number = data.map(toBase10)
+  const base10Sum = _.sum(base10Number)
+  return [base10Sum, toBaseSNAFU(base10Sum)]
 }
 
 console.log("Sample:", [{ data: sample }].map(solve1))
