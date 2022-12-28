02df8947b491d83de19b8960bd500439daa5c37f
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2022-12-29 00:03:28 +0100

    day 16 (cleanup)

--- day16/solve.js
+++ day16/solve.js
@@ -42,63 +42,7 @@ const solve1 = ({ data }) => {
 
 /// Part 2
 
-const solve2A = ({ data }) => {
-  const valves = []
-
-  valves: for (const [id, { flowRate }] of Object.entries(data)) {
-    if (flowRate === 0 && id !== "AA") continue valves
-    const queue = []
-    const seen = {}
-    queue.push([id, 0])
-    valves[id] = { flowRate, nodes: [] }
-
-    bsf: while (queue.length > 0) {
-      const [valveId, distance] = queue.pop()
-      if (valveId in seen) continue bsf
-      seen[valveId] = true
-
-      const { flowRate, nodes } = data[valveId]
-      if (flowRate > 0 && id != valveId) {
-        valves[id].nodes.push([valveId, distance])
-      }
-
-      for (const n of nodes) {
-        queue.unshift([n, distance + 1])
-      }
-    }
-  }
-
-  const cache = []
-
-  const findMaxPressure = (pos, opened, timeLeft) => {
-    if (timeLeft <= 0) return 0
-
-    const key = [pos, opened, timeLeft].join(":")
-    if (key in cache) return cache[key]
-
-    var best = 0
-    const { flowRate, nodes } = valves[pos]
-    const currentRelease = (timeLeft - 1) * flowRate
-
-    for (const next of nodes) {
-      if (currentRelease != 0 && !opened.includes(pos)) {
-        // Open current valve and move to next tunnel
-        const nextRelease = findMaxPressure(next, [...opened, pos], timeLeft - 2)
-        best = _.max([best, currentRelease + nextRelease])
-      }
-
-      // Ignore current valve and continue walking to the next tunnels
-      best = _.max([best, findMaxPressure(next, opened, timeLeft - 1)])
-    }
-
-    cache[key] = best
-    return best
-  }
-
-  return findMaxPressure("AA", [], 30)
-}
-
-const solve2 = ({ data }) => {
+const solve2 = ({ data, minValvesOpen }) => {
   const valves = data
   const cache = []
 
@@ -108,8 +52,9 @@ const solve2 = ({ data }) => {
 
     if (timeLeft == 0 && !elephantIdle) return 0
     if (timeLeft == 0 && elephantIdle) {
-      // Hacky optimization to avoid empty first runs
-      if (opened.length < 6) return 0
+      // Hacky optimization to avoid elephant cycles
+      // where no or too less valves has been opened
+      if (opened.length < minValvesOpen) return 0
       return findMaxPressure("AA", opened, 26, false)
     }
 
@@ -137,5 +82,5 @@ const solve2 = ({ data }) => {
   return findMaxPressure("AA", [], 26, true)
 }
 
-console.log("Sample:", [{ data: data }].map(solve2))
-// console.log("Task:", [{ data: data }].map(solve2))
+console.log("Sample:", [{ data: sample, minValvesOpen: 3 }].map(solve2))
+console.log("Task:", [{ data: data, minValvesOpen: 6 }].map(solve2))
