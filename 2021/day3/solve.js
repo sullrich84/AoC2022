80f4be11fd37fcd92d1adcb0701341af639338c6
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-03 19:25:00 +0100

    day 3

--- 2021/day3/solve.js
+++ 2021/day3/solve.js
@@ -6,16 +6,40 @@ console.log("🎄 Day 3: Binary Diagnostic")
 /// Part 1
 
 const solve1 = ({ data }) => {
-  return 0
+  var gammaRate = ""
+  var epsilonRate = ""
+
+  for (var i = 0; i < data[0].length; i++) {
+    var count = 0
+    for (const bit of data) {
+      count += bit[i] === 0 ? -1 : +1
+    }
+    gammaRate += count > 0 ? "1" : "0"
+    epsilonRate += count > 0 ? "0" : "1"
+  }
+
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 }
 
 console.log("Sample:", [{ data: sample }].map(solve1))
-// console.log("Task:", [{ data: data }].map(solve1))
+console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
 const solve2 = ({ data }) => {
-  return 0
+  var gammaRate = []
+  var epsilonRate = []
+
+  for (var i = 0; i < data[0].length; i++) {
+    var count = 0
+    for (const bit of data) {
+      count += bit[i] === 0 ? -1 : +1
+    }
+    gammaRate += count > 0 ? "1" : "0"
+    epsilonRate += count > 0 ? "0" : "1"
+  }
+
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 }
 
 console.log("Sample:", [{ data: sample }].map(solve2))
