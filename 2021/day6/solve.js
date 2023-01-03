cfe8b3909e3a230473471f05f627a0e595ec36d4
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-01-03 16:07:54 +0100

    day 6

--- 2021/day6/solve.js
+++ 2021/day6/solve.js
@@ -6,11 +6,25 @@ console.log("🎄 Day 6: Lanternfish")
 /// Part 1
 
 const solve1 = ({ data }) => {
-  return 0
+  var state = [...data]
+
+  for (var day = 0; day < 80; day++) {
+    const add = []
+    state = state
+      .map((f) => f - 1)
+      .map((f) => {
+        if (f >= 0) return f
+        add.push(8)
+        return 6
+      })
+    state.push(...add)
+  }
+
+  return state.length
 }
 
 console.log("Sample:", [{ data: sample }].map(solve1))
-// console.log("Task:", [{ data: data }].map(solve1))
+console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
@@ -18,5 +32,5 @@ const solve2 = ({ data }) => {
   return 0
 }
 
-// console.log("Sample:", [{ data: sample }].map(solve2))
-// console.log("Task:", [{ data: data }].map(solve2))
+console.log("Sample:", [{ data: sample }].map(solve2))
+console.log("Task:", [{ data: data }].map(solve2))
