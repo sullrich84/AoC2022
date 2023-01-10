fe3b301e593872975b96162c67af039b963086f4
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-11 00:21:47 +0100

    day 16

---
+++ 2021/day16/solve.js
@@ -0,0 +1,22 @@
+import _ from "lodash"
+import data, { sample } from "./data.js"
+
+console.log("🎄 Day 16: Packet Decoder")
+
+/// Part 1
+
+const solve1 = ({ data }) => {
+  return parseInt("F", 16)
+}
+
+console.log("Sample:", [{ data: sample }].map(solve1))
+// console.log("Task:", [{ data: data }].map(solve1))
+
+/// Part 2
+
+const solve2 = ({ data }) => {
+  return 0
+}
+
+// console.log("Sample:", [{ data: sample }].map(solve2))
+// console.log("Task:", [{ data: data }].map(solve2))
