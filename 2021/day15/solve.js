41837c5d0daac2e926948b237bf44ee5dbe4ae42
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-10 21:36:21 +0100

    day 15

--- 2021/day15/solve.js
+++ 2021/day15/solve.js
@@ -5,19 +5,15 @@ console.log("🎄 Day 15: Chiton")
 
 /// Part 1
 
-const calcDist = ([ay, ax], [by, bx]) => {
-  return Math.abs(ay - by) + Math.abs(ax - bx)
-}
-
 const solve1 = ({ data }) => {
   const h = data.length
   const w = data[0].length
 
   const dirs = [
-    [-1, 0],
-    [0, -1],
-    [0, +1],
-    [+1, 0],
+    [+1, 0], // down
+    [0, +1], // right
+    [-1, 0], // up
+    [0, -1], // left
   ]
 
   const start = [0, 0]
@@ -26,22 +22,19 @@ const solve1 = ({ data }) => {
   const stack = []
   const visited = {}
 
-  var minRisk = 734
-
+  var minRisk = Number.POSITIVE_INFINITY
   stack.push([start, finish, 0])
 
   loop: while (stack.length > 0) {
     const [[py, px], [dy, dx], risk] = stack.pop()
 
     const key = [py, px].join()
-    if (key in visited && visited[key] < risk) continue loop
+    if (key in visited) continue loop
     visited[key] = risk
 
-    if (risk > minRisk) continue loop
-
     if (py === dy && px === dx) {
-      minRisk = Math.min(risk, minRisk)
-      continue loop
+      minRisk = risk
+      break loop
     }
 
     dirs: for (const [y, x] of dirs) {
@@ -51,10 +44,8 @@ const solve1 = ({ data }) => {
       stack.push([[ny, nx], [dy, dx], risk + nRisk])
     }
 
-    // Sort stack to priorize shortest distances
-    stack.sort((a, b) => {
-      return b[2] - a[2]
-    })
+    // Priorize by sorting risk descending (lowest risk last)
+    stack.sort((a, b) => b[2] - a[2])
   }
 
   return minRisk
@@ -66,8 +57,21 @@ console.log("Task:", [{ data: data }].map(solve1))
 /// Part 2
 
 const solve2 = ({ data }) => {
-  return 0
+  const h = data.length
+  const w = data[0].length
+  const data5x = _.times(h * 5, (y) => _.times(w * 5, (x) => data[y % h][x % w]))
+
+  for (var y = 0; y < data.length * 5; y++) {
+    for (var x = 0; x < data[0].length * 5; x++) {
+      if (y < h && x < w) continue
+      const [my, mx] = [y % h, x % w]
+      const val = data[my][mx]
+      data5x[y][x] = ((val + (Math.floor(x / w) + Math.floor(y / h)) - 1) % 9) + 1
+    }
+  }
+
+  return solve1({ data: data5x })
 }
 
-// console.log("Sample:", [{ data: sample }].map(solve2))
-// console.log("Task:", [{ data: data }].map(solve2))
+console.log("Sample:", [{ data: sample }].map(solve2))
+console.log("Task:", [{ data: data }].map(solve2))
