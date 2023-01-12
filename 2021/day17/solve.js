976c01655675b92cca0d03e2f183bd59e348acb9
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-01-12 10:23:24 +0100

    day 17

--- 2021/day17/solve.js
+++ 2021/day17/solve.js
@@ -3,63 +3,73 @@ import data, { sample } from "./data.js"
 
 console.log("🎄 Day 17: Trick Shot")
 
+const shoot = ([y, x], [txStart, txEnd], [tyStart, tyEnd]) => {
+  var [vy, vx] = [y, x]
+  var [py, px] = [0, 0]
+  const trajectory = []
+
+  while (true) {
+    var [ny, nx] = [py + vy, px + vx]
+    trajectory.push([ny, nx])
+
+    // Check if hit
+    if (ny <= tyStart && ny >= tyEnd && nx >= txStart && nx <= txEnd) {
+      return trajectory
+    }
+
+    // Check if missed
+    if (nx > txEnd) return "miss"
+    if (ny < tyEnd) return "miss"
+
+    // Apply drag
+    vx = vx === 0 ? 0 : vx > 0 ? vx - 1 : vx + 1
+    vy -= 1
+
+    // Update next position
+    ;[py, px] = [ny, nx]
+  }
+}
+
 /// Part 1
 
 const solve1 = ({ data }) => {
   const [[txStart, txEnd], [tyStart, tyEnd]] = data
-  const trajectories = []
-  const history = {}
+  var lastMaxY = 0
 
-  for (var y = 0; y < 9000; y++) {
-    for (var x = 1; x <= txEnd; x++) {
-      var [vy, vx] = [y, x]
-      var [py, px] = [0, 0]
-      const trajectory = []
-      // console.log("Testing angle ", [vy, vx])
+  for (var y = 1; y < 100; y++) {
+    x: for (var x = txEnd; x > 0; x--) {
+      const outcome = shoot([y, x], [txStart, txEnd], [tyStart, tyEnd])
+      if (outcome === "miss") continue x
 
-      sim: for (var s = 0; s < 100; s++) {
-        var [ny, nx] = [py + vy, px + vx]
-        // console.log("Probe at", [ny, nx])
-        trajectory.push([ny, nx])
-
-        // Check if hit
-        if (ny <= tyStart && ny >= tyEnd && nx >= txStart && nx <= txEnd) {
-          // console.log("HIT!")
-          trajectories.push(trajectory)
-          history[`${y}, ${x}`] = _.max(trajectory.map(([y]) => y))
-          break sim
-        }
-
-        // Check if missed
-        if (ny < tyEnd) {
-          // console.log("MISS! (too low)")
-          break sim
-        }
-
-        if (nx > txEnd) {
-          // console.log("MISS! (too high)")
-          break sim
-        }
-
-        // Apply drag
-        vx = vx === 0 ? 0 : vx > 0 ? vx - 1 : vx + 1
-        vy -= 1
-        ;[py, px] = [ny, nx]
+      const maxY = _.max(outcome.map(([y]) => y))
+      if (maxY > lastMaxY) {
+        lastMaxY = maxY
       }
     }
   }
 
-  return _.max(trajectories.map((t) => _.max(t.map(([y]) => y))))
+  return lastMaxY
 }
 
-// console.log("Sample:", [{ data: sample }].map(solve1))
+console.log("Sample:", [{ data: sample }].map(solve1))
 console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
 const solve2 = ({ data }) => {
-  return 0
+  const [[txStart, txEnd], [tyStart, tyEnd]] = data
+  var hitConfig = []
+
+  for (var y = -100; y < 100; y++) {
+    x: for (var x = txEnd; x > 0; x--) {
+      const outcome = shoot([y, x], [txStart, txEnd], [tyStart, tyEnd])
+      if (outcome === "miss") continue x
+      hitConfig.push([y, x])
+    }
+  }
+
+  return hitConfig.length
 }
 
-// console.log("Sample:", [{ data: sample }].map(solve2))
-// console.log("Task:", [{ data: data }].map(solve2))
+console.log("Sample:", [{ data: sample }].map(solve2))
+console.log("Task:", [{ data: data }].map(solve2))
