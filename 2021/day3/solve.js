2c5dbd7256e681391a2e221b41314f4698e1d778
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-01-17 14:29:29 +0100

    day 3

--- 2021/day3/solve.js
+++ 2021/day3/solve.js
@@ -8,14 +8,12 @@ console.log("🎄 Day 3: Binary Diagnostic")
 const solve1 = ({ data }) => {
   var gammaRate = ""
   var epsilonRate = ""
+  const half = data.length / 2
 
   for (var i = 0; i < data[0].length; i++) {
-    var count = 0
-    for (const bit of data) {
-      count += bit[i] === 0 ? -1 : +1
-    }
-    gammaRate += count > 0 ? "1" : "0"
-    epsilonRate += count > 0 ? "0" : "1"
+    var count = _.sum(data.map((r) => r[i]))
+    gammaRate += count > half ? 1 : 0
+    epsilonRate += count > half ? 0 : 1
   }
 
   return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
@@ -27,19 +25,30 @@ console.log("Task:", [{ data: data }].map(solve1))
 /// Part 2
 
 const solve2 = ({ data }) => {
-  var gammaRate = []
-  var epsilonRate = []
+  var oxygenRate = [...data]
+  var co2Rate = [...data]
 
   for (var i = 0; i < data[0].length; i++) {
-    var count = 0
-    for (const bit of data) {
-      count += bit[i] === 0 ? -1 : +1
+    const sum = _.sum(oxygenRate.map((r) => r[i]))
+    const bit = sum >= oxygenRate.length / 2 ? 1 : 0
+    oxygenRate = oxygenRate.filter((r) => r[i] === bit)
+    if (oxygenRate.length === 1) {
+      oxygenRate = oxygenRate[0].join("")
+      break
     }
-    gammaRate += count > 0 ? "1" : "0"
-    epsilonRate += count > 0 ? "0" : "1"
   }
 
-  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
+  for (var i = 0; i < data[0].length; i++) {
+    const sum = _.sum(co2Rate.map((r) => r[i]))
+    const bit = sum >= co2Rate.length / 2 ? 0 : 1
+    co2Rate = co2Rate.filter((r) => r[i] === bit)
+    if (co2Rate.length === 1) {
+      co2Rate = co2Rate[0].join("")
+      break
+    }
+  }
+
+  return parseInt(oxygenRate, 2) * parseInt(co2Rate, 2)
 }
 
 console.log("Sample:", [{ data: sample }].map(solve2))
