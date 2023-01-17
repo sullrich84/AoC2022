1a570ecaa37162f5bfe921cdb738263fc819b2bb
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-01-17 11:59:17 +0100

    day 21

--- 2021/day21/solve.js
+++ 2021/day21/solve.js
@@ -33,8 +33,8 @@ const solve1 = ([startP1, startP2]) => {
   return Math.min(players.p1.score, players.p2.score) * rolls
 }
 
-// console.log("Sample:", solve1(sample))
-// console.log("Task:", solve1(data))
+console.log("Sample:", solve1(sample))
+console.log("Task:", solve1(data))
 
 /// Part 2
 
@@ -54,14 +54,13 @@ const solve2 = ([startP1, startP2]) => {
   const play = ({ p1, p2, turn }) => {
     const key = [p1.score, p1.pos, p2.score, p2.pos, turn].join()
     if (key in cache) return cache[key]
-
     var [p1Wins, p2Wins] = [0, 0]
 
     if (turn === 0) {
       // Turn: Player 1
       for (const r of rolls) {
         const posP1 = ((p1.pos + r - 1) % 10) + 1
-        const scoreP1 = p1.pos + p1.score
+        const scoreP1 = p1.score + posP1
 
         if (scoreP1 >= 21) {
           p1Wins += 1
@@ -75,7 +74,7 @@ const solve2 = ([startP1, startP2]) => {
       // Turn: Player 2
       for (const r of rolls) {
         const posP2 = ((p2.pos + r - 1) % 10) + 1
-        const scoreP2 = p2.pos + p2.score
+        const scoreP2 = p2.score + posP2
 
         if (scoreP2 >= 21) {
           p2Wins += 1
@@ -91,17 +90,14 @@ const solve2 = ([startP1, startP2]) => {
     return [p1Wins, p2Wins]
   }
 
-  // 444356092776315
-  // 14598271321389
-
-  const result = play({
-    p1: { score: 0, pos: startP1 },
-    p2: { score: 0, pos: startP2 },
-    turn: 0,
-  })
-
-  return result
+  return _.max(
+    play({
+      p1: { score: 0, pos: startP1 },
+      p2: { score: 0, pos: startP2 },
+      turn: 0,
+    }),
+  )
 }
 
 console.log("Sample:", solve2(sample))
-// console.log("Task:", solve2(data))
+console.log("Task:", solve2(data))
