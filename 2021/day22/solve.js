0fb5829490600d4fbb64a8608b6c2fc6e11ac5f9
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-18 23:47:07 +0100

    day 22

--- 2021/day22/solve.js
+++ 2021/day22/solve.js
@@ -33,8 +33,26 @@ console.log("Task:", [{ data: data }].map(solve1))
 /// Part 2
 
 const solve2 = ({ data }) => {
-  return 0
+  const grid = new Set()
+
+  for (const [active, xr, yr, zr] of data) {
+    const [xs, xe] = [Math.max(xr[0], -50), Math.min(xr[1], 50)]
+    const [ys, ye] = [Math.max(yr[0], -50), Math.min(yr[1], 50)]
+    const [zs, ze] = [Math.max(zr[0], -50), Math.min(zr[1], 50)]
+
+    for (var x = xs; x <= xe; x++) {
+      for (var y = ys; y <= ye; y++) {
+        for (var z = zs; z <= ze; z++) {
+          const key = [x, y, z].join(":")
+          if (active) grid.add(key)
+          else grid.delete(key)
+        }
+      }
+    }
+  }
+
+  return grid.size
 }
 
-// console.log("Sample:", [{ data: sample }].map(solve2))
+console.log("Sample:", [{ data: sample }].map(solve2))
 // console.log("Task:", [{ data: data }].map(solve2))
