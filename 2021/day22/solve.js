5d69408fbc89e78ae737053ea71a11e9ba302283
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-19 20:39:17 +0100

    day 22

--- 2021/day22/solve.js
+++ 2021/day22/solve.js
@@ -1,5 +1,5 @@
 import _ from "lodash"
-import data, { sample } from "./data.js"
+import data, { sample, smallSample } from "./data.js"
 
 console.log("🎄 Day 22: Reactor Reboot")
 
@@ -27,32 +27,31 @@ const solve1 = ({ data }) => {
   return grid.size
 }
 
-console.log("Sample:", [{ data: sample }].map(solve1))
-console.log("Task:", [{ data: data }].map(solve1))
+// console.log("Sample:", [{ data: sample }].map(solve1))
+// console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
 const solve2 = ({ data }) => {
-  const grid = new Set()
+  var activeCells = 0
 
-  for (const [active, xr, yr, zr] of data) {
-    const [xs, xe] = [Math.max(xr[0], -50), Math.min(xr[1], 50)]
-    const [ys, ye] = [Math.max(yr[0], -50), Math.min(yr[1], 50)]
-    const [zs, ze] = [Math.max(zr[0], -50), Math.min(zr[1], 50)]
+  const xr = _.flatten(data.map((e) => e[1]))
+  const yr = _.flatten(data.map((e) => e[2]))
+  const zr = _.flatten(data.map((e) => e[3]))
 
-    for (var x = xs; x <= xe; x++) {
-      for (var y = ys; y <= ye; y++) {
-        for (var z = zs; z <= ze; z++) {
-          const key = [x, y, z].join(":")
-          if (active) grid.add(key)
-          else grid.delete(key)
-        }
-      }
-    }
+  const [minX, maxX] = [_.min(xr), _.max(xr)]
+  const [minY, maxY] = [_.min(yr), _.max(yr)]
+  const [minZ, maxZ] = [_.min(zr), _.max(zr)]
+
+  const space = (Math.abs(maxX - minX) + 1) * (Math.abs(maxY - minY) + 1) * (Math.abs(maxZ - minZ) + 1)
+
+  for (const [active, [sx, ex], [sy, ey], [sz, ez]] of data) {
+    const sum = (Math.abs(ex - sx) + 1) * (Math.abs(ey - sy) + 1) * (Math.abs(ez - sz) + 1)
+    console.log(active, space, sum)
   }
 
-  return grid.size
+  return activeCells
 }
 
-console.log("Sample:", [{ data: sample }].map(solve2))
+console.log("Sample:", [{ data: smallSample }].map(solve2))
 // console.log("Task:", [{ data: data }].map(solve2))
