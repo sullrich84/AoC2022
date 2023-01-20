095cb215ae052e751682272991f7963f3cdf204e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-01-20 16:56:32 +0100

    day 22

--- 2021/day22/solve.js
+++ 2021/day22/solve.js
@@ -1,5 +1,5 @@
 import _ from "lodash"
-import data, { sample, smallSample } from "./data.js"
+import data, { sample, sample2 } from "./data.js"
 
 console.log("🎄 Day 22: Reactor Reboot")
 
@@ -33,25 +33,33 @@ const solve1 = ({ data }) => {
 /// Part 2
 
 const solve2 = ({ data }) => {
-  var activeCells = 0
-
-  const xr = _.flatten(data.map((e) => e[1]))
-  const yr = _.flatten(data.map((e) => e[2]))
-  const zr = _.flatten(data.map((e) => e[3]))
-
-  const [minX, maxX] = [_.min(xr), _.max(xr)]
-  const [minY, maxY] = [_.min(yr), _.max(yr)]
-  const [minZ, maxZ] = [_.min(zr), _.max(zr)]
-
-  const space = (Math.abs(maxX - minX) + 1) * (Math.abs(maxY - minY) + 1) * (Math.abs(maxZ - minZ) + 1)
-
-  for (const [active, [sx, ex], [sy, ey], [sz, ez]] of data) {
-    const sum = (Math.abs(ex - sx) + 1) * (Math.abs(ey - sy) + 1) * (Math.abs(ez - sz) + 1)
-    console.log(active, space, sum)
+  const volume = ([[xs, xe], [ys, ye], [zs, ze]]) => {
+    return (Math.abs(xe - xs) + 1) * (Math.abs(ye - ys) + 1) * (Math.abs(ze - zs) + 1)
   }
 
-  return activeCells
+  const intersection = ([[axs, axe], [ays, aye], [azs, aze]], [[bxs, bxe], [bys, bye], [bzs, bze]]) => {
+    if (axe < bxs || bxe < axs || aye < bys || bye < ays || aze < bzs || bze < azs) return []
+    const xr = [Math.max(axs, bxs), Math.min(axe, bxe)]
+    const yr = [Math.max(ays, bys), Math.min(aye, bye)]
+    const zr = [Math.max(azs, bzs), Math.min(aze, bze)]
+    return [xr, yr, zr]
+  }
+
+  const cubes = []
+
+  for (const cubeA of data) {
+    const [aa, ax, ay, az] = cubeA
+
+    const intersects = cubes
+      .map(([ba, bx, by, bz]) => [ba, ...intersection([ax, ay, az], [bx, by, bz])])
+      .filter(([_, ...e]) => !e)
+
+    if (aa) cubes.push(cubeA)
+    cubes.push(...intersects.map(([ia, ...i]) => [(aa && !ia) || (!aa && !ia), ...i]))
+  }
+
+  return cubes.map(([ea, ...e]) => (ea ? volume(e) : -volume(e))).reduce((p, c) => p + c, 0)
 }
 
-console.log("Sample:", [{ data: smallSample }].map(solve2))
-// console.log("Task:", [{ data: data }].map(solve2))
+console.log("Sample:", [{ data: sample2 }].map(solve2))
+console.log("Task:", [{ data: data }].map(solve2))
