27211838396a11677a1837c83c4a25f7f15c06fe
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-02-03 23:26:35 +0100

    day 24

--- 2021/day24/solve.js
+++ 2021/day24/solve.js
@@ -1,57 +1,59 @@
 import _ from "lodash"
-import data, { sample } from "./data.js"
+import data, { sample, data2 } from "./data.js"
 
 console.log("🎄 Day 24: Arithmetic Logic Unit")
 
 /// Part 1
 
 const ops = {
-  add: (v1, v2) => v1 + v2,
-  mul: (v1, v2) => v1 * v2,
-  div: (v1, v2) => {
-    if (v2 === 0) throw new Error("Invalid division")
-    return Math.floor(v1 / v2)
-  },
-  mod: (v1, v2) => {
-    if (v1 < 0 || v2 <= 0) throw new Error("Invalid modulo")
-    return v1 % v2
-  },
-  eql: (v1, v2) => (v1 === v2 ? 1 : 0),
+  add: (a, b) => a + b,
+  mul: (a, b) => a * b,
+  div: (a, b) => (a / b) | 0,
+  mod: (a, b) => a % b,
+  eql: (a, b) => (a === b ? 1 : 0),
+}
+
+const calcMonad = (number, instructions, x = 0, y = 0, z = 0) => {
+  const res = { w: number, x, y, z }
+
+  for (const [op, v1, v2] of instructions) {
+    if (op === "inp") res[v1] = number
+    else res[v1] = ops[op](res[v1], v2 in res ? res[v2] : v2)
+  }
+
+  return res.z
 }
 
 const solve1 = ({ data }) => {
   const validCache = {}
+  const instructions = _.chunk(data, 18)
+  const lookup = _.times(instructions.length, () => ({ w: [], x: [], y: [], z: [] }))
 
-  function isValid(number) {
-    const res = { w: 0, x: 0, y: 0, z: 0 }
-
-    const numStr = number.toString()
-    if (numStr.length !== 14 || numStr.includes("0")) return false
-    var monad = numStr.split("").map((c) => parseInt(c))
-
-    for (const [op, v1, v2] of data) {
-      if (op === "inp") res[v1] = monad.shift()
-      else res[v1] = ops[op](res[v1], v2 in res ? res[v2] : v2)
+  for (var i = 0; i < instructions.length; i++) {
+    for (var w = 1; w <= 9; w++) {
+      for (var x = -26; x <= 26; x++) {
+        for (var y = -26; y <= 26; y++) {
+          for (var z = -26; z <= 26; z++) {
+            const result = calcMonad(w, instructions[i], x, y, z)
+            if (result === 0) {
+              lookup[i] = {
+                w: _.uniq([...lookup[i].w, w]),
+                x: _.uniq([...lookup[i].x, x]),
+                y: _.uniq([...lookup[i].y, y]),
+                z: _.uniq([...lookup[i].z, z]),
+              }
+            }
+          }
+        }
+      }
     }
-
-    return res.z
   }
 
-  const [start, end] = [11111111111111, 99999999999999]
+  // lookup.w = _.uniq(lookup.w)
+  // lookup.x = _.uniq(lookup.x)
+  // lookup.y = _.uniq(lookup.y)
+  // lookup.z = _.uniq(lookup.z)
 
-  for (var i = start; i < end; i++) {
-    if (isValid(i) === 0) debugger
-  }
-
-  // 99999999999999 = 7645130736
-  // 88888888888888 = 7323858329
-  // 77777777777777 = 7002585922
-  // 66666666666666 = 6681313515
-  // 55555555555555 = 6360041108
-  // 44444444444444 = 6038768701
-  // 33333333333333 = 5717496294
-  // 22222222222222 = 5396223887
-  // 11111111111111 = 5396223887
   return
 }
 
