d9a74d84c70ea53467f7d04e2c676b74b9ae7919
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-02-07 00:13:27 +0100

    day 25

--- 2021/day25/solve.js
+++ 2021/day25/solve.js
@@ -5,12 +5,75 @@ console.log("🎄 Day 25: Sea Cucumber")
 
 /// Part 1
 
+function debug(map) {
+  return map.map((r) => r.map((e) => [".", ">", "v"][e]).join(""))
+}
+
 const solve1 = ({ data }) => {
-  return 0
+  // East movers = 1
+  // South mover = 2
+  // Empty space = 0
+
+  const [w, h] = [data[0].length, data.length]
+
+  var steps = 0
+  var done = false
+  var map = _.cloneDeep(data)
+
+  while (!done) {
+    const dMap = debug(map)
+    var moved = 0
+
+    var eastMovedMap = _.cloneDeep(map)
+
+    // Move east movers
+    for (var y = 0; y < h; y++) {
+      for (var x = 0; x < w; x++) {
+        const p = _.get(map, [y, x])
+        if (p !== 1) continue
+
+        // Move right
+        const nx = x + 1 === w ? 0 : x + 1
+        const n = _.get(map, [y, nx])
+        if (n !== 0) continue
+
+        _.set(eastMovedMap, [y, x], 0)
+        _.set(eastMovedMap, [y, nx], 1)
+        moved += 1
+      }
+    }
+
+    var map = _.cloneDeep(eastMovedMap)
+    var southMovedMap = _.cloneDeep(map)
+
+    // Move south movers
+    for (var y = 0; y < h; y++) {
+      for (var x = 0; x < w; x++) {
+        const p = _.get(map, [y, x])
+        if (p !== 2) continue
+
+        // Move down
+        const ny = y + 1 === h ? 0 : y + 1
+        const n = _.get(map, [ny, x])
+        if (n !== 0) continue
+
+        _.set(southMovedMap, [y, x], 0)
+        _.set(southMovedMap, [ny, x], 2)
+        moved += 1
+      }
+    }
+
+    map = _.cloneDeep(southMovedMap)
+
+    steps += 1
+    done = moved === 0
+  }
+
+  return steps
 }
 
 console.log("Sample:", [{ data: sample }].map(solve1))
-// console.log("Task:", [{ data: data }].map(solve1))
+console.log("Task:", [{ data: data }].map(solve1))
 
 /// Part 2
 
