f4c17cd7178bd13b8d0b1fcd0459122423e2b6e8
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-02-10 19:13:05 +0100

    day 19

--- 2021/day19/solve.js
+++ 2021/day19/solve.js
@@ -6,7 +6,48 @@ console.log("🎄 Day 19: Beacon Scanner")
 /// Part 1
 
 const solve1 = ({ data }) => {
-  return 0
+  const hashes = []
+  const totalBeacons = data[0].length * data.length
+
+  for (var s = 0; s < data.length; s++) {
+    const beacons = data[s]
+    for (var b = 0; b < beacons.length; b++) {
+      // Calculate distances between beacons to use them as some sort of hash
+      const hash = []
+      const [cx, cy, cz] = beacons[b]
+
+      for (var bn = 0; bn < beacons.length; bn++) {
+        if (b === bn) continue
+        const [nx, ny, nz] = beacons[bn]
+
+        const [xDist, yDist, zDist] = [Math.abs(cx - nx), Math.abs(cy - ny), Math.abs(cz - nz)]
+        const key = [xDist, yDist, zDist].sort((a, b) => b - a).join("-")
+
+        hash.push(key)
+      }
+
+      hashes.push(hash)
+    }
+  }
+
+  // Find duplicate beacons by comparing hashes
+  for (var s = 0; s < data.length; s++) {
+    const beacons = hashes[s]
+    for (var b = 0; b < beacons.length; b++) {
+      // Calculate distances between beacons to use them as some sort of hash
+      for (var bn = 0; bn < beacons.length; bn++) {
+        if (b === bn) continue
+        const [nx, ny, nz] = beacons[bn]
+
+        const [xDist, yDist, zDist] = [Math.abs(cx - nx), Math.abs(cy - ny), Math.abs(cz - nz)]
+        const key = [xDist, yDist, zDist].sort((a, b) => b - a).join("-")
+
+        hash.push(key)
+      }
+    }
+  }
+
+  return totalBeacons - duplicates
 }
 
 console.log("Sample:", [{ data: sample }].map(solve1))
