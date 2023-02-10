bf6bbabd1cddfb465bc0465eff78a3b0c0979ce8
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-02-10 20:49:47 +0100

    day 19

--- 2021/day19/solve.js
+++ 2021/day19/solve.js
@@ -8,25 +8,55 @@ console.log("🎄 Day 19: Beacon Scanner")
 const solve1 = ({ data }) => {
   const hashes = []
   const totalBeacons = data[0].length * data.length
+  const rotate = ([x, y, z]) => [
+    [x, y, z],
+    [x, z, -y],
+    [x, -y, -z],
+    [x, -z, y],
+    [-x, -y, z],
+    [-x, z, y],
+    [-x, y, -z],
+    [-x, -z, -y],
+    [y, z, x],
+    [y, x, -z],
+    [y, -z, -x],
+    [y, -x, z],
+    [-y, -z, x],
+    [-y, x, z],
+    [-y, z, -x],
+    [-y, -x, -z],
+    [z, x, y],
+    [z, y, -x],
+    [z, -x, -y],
+    [z, -y, x],
+    [-z, -x, y],
+    [-z, y, x],
+    [-z, x, -y],
+    [-z, -y, -x],
+  ]
 
   for (var s = 0; s < data.length; s++) {
     const beacons = data[s]
     for (var b = 0; b < beacons.length; b++) {
       // Calculate distances between beacons to use them as some sort of hash
-      const hash = []
       const [cx, cy, cz] = beacons[b]
+      const cubeHash = []
 
       for (var bn = 0; bn < beacons.length; bn++) {
         if (b === bn) continue
         const [nx, ny, nz] = beacons[bn]
+        const rotations = rotate([nx, ny, nz])
+        const hash = []
 
-        const [xDist, yDist, zDist] = [Math.abs(cx - nx), Math.abs(cy - ny), Math.abs(cz - nz)]
-        const key = [xDist, yDist, zDist].sort((a, b) => b - a).join("-")
-
-        hash.push(key)
+        // Rotate point to all posible rotations
+        for (const [rx, ry, rz] of rotations) {
+          const [xDist, yDist, zDist] = [Math.abs(cx - rx), Math.abs(cy - ry), Math.abs(cz - rz)]
+          const key = [xDist, yDist, zDist].sort((a, b) => b - a).join("-")
+          hash.push(key)
+        }
+        cubeHash.push(hash)
       }
-
-      hashes.push(hash)
+      hashes.push(cubeHash)
     }
   }
 
@@ -35,15 +65,7 @@ const solve1 = ({ data }) => {
     const beacons = hashes[s]
     for (var b = 0; b < beacons.length; b++) {
       // Calculate distances between beacons to use them as some sort of hash
-      for (var bn = 0; bn < beacons.length; bn++) {
-        if (b === bn) continue
-        const [nx, ny, nz] = beacons[bn]
-
-        const [xDist, yDist, zDist] = [Math.abs(cx - nx), Math.abs(cy - ny), Math.abs(cz - nz)]
-        const key = [xDist, yDist, zDist].sort((a, b) => b - a).join("-")
-
-        hash.push(key)
-      }
+      for (var bn = 0; bn < beacons.length; bn++) {}
     }
   }
 
